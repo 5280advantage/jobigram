25112ed39551bce2936fb34ce8cab99e6481c35f
guru <gurupratap_123@yahoo.com>
2016-11-18 19:37:04 +0530

    test branch

--- www/app/directive/photoLikeDirective.js
+++ www/app/directive/photoLikeDirective.js
@@ -10,9 +10,12 @@
                 loading: '=',
                 action: '@'
             },
+            
+            
             link: function (scope, elem) {
                 elem.bind('click', function () {
                         if (scope.action == 'apply') {
+                            console.log('test branch')
                             if (scope.loading) return;
                             var _model = scope.ngModel;
                             _model.progress = true;
